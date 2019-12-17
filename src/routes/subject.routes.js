1dbecc8cc26a506818bbff86fb622506ab35df30
thang <thang13199@gmail.com>
2019-12-17 23:41:05 +0700

    thang b1

--- src/routes/subject.routes.js
+++ src/routes/subject.routes.js
@@ -5,8 +5,8 @@ const tokencheck=require("../middlewares/tokens.middlewares");
 const role=require("../middlewares/role.middlewares");
 
 router.post("/create", tokencheck.verify,role.verify_isNotAdmin,subjectController.createSubject);
-router.post("/delete", subjectController.deleteSubject);
-router.post("/update", subjectController.updateSubject);
+router.post("/delete", tokencheck.verify,role.verify_isNotAdmin,subjectController.deleteSubject);
+router.post("/update", tokencheck.verify,role.verify_isNotAdmin,subjectController.updateSubject);
 
 
 module.exports = router;
